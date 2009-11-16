33f1ac6aaaf4319b7627ac2b00c7806db9afd9fa
anaue.costa <anaue.costa@d1dce0c0-ae44-11de-9c47-3f15b72ea178>
2009-11-16 21:11:56 +0000

    mudanças no sintatic

--- trunk/CompilatorLivePlus/CompilatorLivePlus/Sintatic/Sintatic.cs
+++ trunk/CompilatorLivePlus/CompilatorLivePlus/Sintatic/Sintatic.cs
@@ -1,10 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CompilatorLivePlus.Sintatic
 {
-    public class Sintatic
-    {
+            class Sintatic
+            {
+                StreamReader sr;
+                Input.Input inChain;
+                //FileStream rulez;
+                CompilatorLivePlus.Lexer.Lexer _lex;
+                public CompilatorLivePlus.Symbols.Env _environment;
+
+
+                public Sintatic(CompilatorLivePlus.Lexer.Lexer lex)
+                {
+                    _lex = lex;
+                    _environment = new CompilatorLivePlus.Symbols.Env( null);
+
+                    //rulez = new FileStream("regras-codigo.txt", FileMode.OpenOrCreate, FileAccess.Read);
+                }
+
+                public void Program()
+                {
+                    inChain = new CompilatorLivePlus.Input.Input();
+
+                    while (!_lex.hasEnded())
+                    {
+                        Lexer.Token escan = _lex.scan();
+                        inChain.Add(escan);
+                    }
+                    //sr = new StreamReader(rulez);
+
+                    if (inChain.hasNext())
+                    {
+                        Console.WriteLine(" ");
+                    }
+                }
+
     }
 }

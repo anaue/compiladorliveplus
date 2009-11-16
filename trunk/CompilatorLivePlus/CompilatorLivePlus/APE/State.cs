590c5acf7b4216abf5ff9a2323e587621b643b6e
fgutiyama <fgutiyama@d1dce0c0-ae44-11de-9c47-3f15b72ea178>
2009-11-16 19:35:57 +0000

    Sintatic and APE structures added

---
+++ trunk/CompilatorLivePlus/CompilatorLivePlus/APE/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CompilatorLivePlus.Lexer;
+
+namespace CompilatorLivePlus.APE
+{
+    public class State
+    {
+        public int Id;
+        public List<Transition> Transitions;
+        public bool finalState;
+
+        public State getNextState(Token token)
+        {
+            foreach (Transition item in Transitions)
+            {
+                if (item.Input.tag == token.tag)
+                    return item.NextState;
+            }
+            return null;
+        }
+
+    }
+}

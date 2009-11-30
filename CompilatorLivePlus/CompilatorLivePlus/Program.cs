267dcc4493ddc2b6f091d3caf4215483e519fee9
anaue.costa@gmail.com <anaue.costa@gmail.com@d1dce0c0-ae44-11de-9c47-3f15b72ea178>
2009-11-30 16:13:49 +0000

    rodando da mvn tbm

--- CompilatorLivePlus/CompilatorLivePlus/Program.cs
+++ CompilatorLivePlus/CompilatorLivePlus/Program.cs
@@ -51,17 +51,32 @@ namespace CompilatorLivePlus
 
                 Console.WriteLine("Press enter to start MVN execution...");
                 Console.ReadLine();
-                System.Diagnostics.ProcessStartInfo procInfo = new System.Diagnostics.ProcessStartInfo("java -jar mvn\\montador.jar Output\\mvn.txt");
+                System.Diagnostics.ProcessStartInfo procInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c java -jar mvn\\montador.jar Output\\mvn.txt");
                 procInfo.RedirectStandardOutput = true;
                 procInfo.UseShellExecute = false;
                 procInfo.CreateNoWindow = true;
+
 
                 System.Diagnostics.Process process = new System.Diagnostics.Process();
                 process.StartInfo = procInfo;
                 process.Start();
 
+
                 string result = process.StandardOutput.ReadToEnd();
 
+                Console.WriteLine(result);
+                //Console.ReadLine();
+
+                procInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c java -jar mvn\\mvnpcs.jar");
+                procInfo.RedirectStandardOutput = false;
+                procInfo.UseShellExecute = false;
+                procInfo.CreateNoWindow = false;
+
+                process.Dispose();
+                process = new System.Diagnostics.Process();
+                process.StartInfo = procInfo;
+                process.Start();
+                
                 Console.WriteLine(result);
                 Console.ReadLine();
 

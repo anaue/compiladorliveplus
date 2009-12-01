45909c49a584d959085f12ea2b44ecb4d1792c94
anaue.costa@gmail.com <anaue.costa@gmail.com@d1dce0c0-ae44-11de-9c47-3f15b72ea178>
2009-12-01 22:24:33 +0000

    symbol

--- CompilatorLivePlus/CompilerModel/Symbols/Symbol.cs
+++ CompilatorLivePlus/CompilerModel/Symbols/Symbol.cs
@@ -8,6 +8,25 @@ namespace CompilerModel.Symbols
 {
     public class Symbol
     {
+        public Symbol(Token _tok)
+        {
+            this.Token = _tok;
+            this.Id = _tok.ToString();
+            this.Name = _tok.ToString();
+
+            if (typeof(Word) == _tok.GetType())
+                this.OperationalValue = ((Word)_tok).Lexeme;
+            else if(typeof(Num) == _tok.GetType())
+                this.OperationalValue = ((Num)_tok).Value.ToString();
+            else
+                this.OperationalValue = _tok.ToString();
+            
+            if (typeof(string) == this.OperationalValue.GetType())
+                this.TargetName = (string)this.OperationalValue;
+
+        }
+        public Symbol() { }
+
         public string Id;
         public Token Token;
         public string Name;
